25d929badb5f1c33c7fbe995f902503a9a168dcf
Ana Kelle Ribeiro <akelly895@gmail.com>
2025-10-03 20:49:34 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,30 +1,8 @@
 # Game777game
-Hangman Game 
-import streamlit as st
-import random
-
-palavras = ["perfume", "banana", "python", "computador", "praia", "tenis"]
-
-
-if "palavra" not in st.session_state:
-    st.session_state.palavra = random.choice(palavras)
-    st.session_state.letras = []
-    st.session_state.tentativas = 6
-
-palavra = st.session_state.palavra
-letras = st.session_state.letras
-tentativas = st.session_state.tentativas
-
-
-palavra_formada = "".join([letra if letra in letras else "*" for letra in palavra])
-
 st.title("🎮 Jogo da Forca")
-st.write(f"Palavra: {palavra_formada}")
-st.write(f"Tentativas restantes: {tentativas}")
 
 
 letra = st.text_input("Digite uma letra: ").lower()
-
 if st.button("Enviar letra"):
     if letra and letra not in letras:
         letras.append(letra)
@@ -32,12 +10,19 @@ if st.button("Enviar letra"):
             st.session_state.tentativas -= 1
 
 
+palavra_formada = "".join([l if l in letras else "*" for l in palavra])
+
+st.write(f"Palavra: {palavra_formada}")
+st.write(f"Tentativas restantes: {st.session_state.tentativas}")
+
+
 if palavra_formada == palavra:
     st.success("🎉 Você ganhou!")
     if st.button("Jogar novamente"):
-        st.session_state.clear()
+        reset_game()
+
 
 elif st.session_state.tentativas == 0:
     st.error(f"💀 Você perdeu! A palavra era: {palavra}")
     if st.button("Jogar novamente"):
-        st.session_state.clear()
+        reset_game()

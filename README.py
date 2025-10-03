2d4cf5705e464e6f597c46b8e9e7e4f247235f15
Ana Kelle Ribeiro <akelly895@gmail.com>
2025-10-03 20:55:09 +0100

    New_README.py

--- README.py
+++ README.py
@@ -1,6 +1,22 @@
 # Game777game
-st.title("🎮 Jogo da Forca")
+import streamlit as st
+import random
 
+palavras = ["perfume", "banana", "python", "computador", "praia", "tenis"]
+
+def reset_game():
+    st.session_state.palavra = random.choice(palavras)
+    st.session_state.letras = []
+    st.session_state.tentativas = 6
+
+if "palavra" not in st.session_state:
+    reset_game()
+
+palavra = st.session_state.palavra
+letras = st.session_state.letras
+tentativas = st.session_state.tentativas
+
+st.title("🎮 Jogo da Forca")
 
 letra = st.text_input("Digite uma letra: ").lower()
 if st.button("Enviar letra"):
@@ -9,18 +25,15 @@ if st.button("Enviar letra"):
         if letra not in palavra:
             st.session_state.tentativas -= 1
 
-
 palavra_formada = "".join([l if l in letras else "*" for l in palavra])
 
 st.write(f"Palavra: {palavra_formada}")
 st.write(f"Tentativas restantes: {st.session_state.tentativas}")
 
-
 if palavra_formada == palavra:
     st.success("🎉 Você ganhou!")
     if st.button("Jogar novamente"):
         reset_game()
-
 
 elif st.session_state.tentativas == 0:
     st.error(f"💀 Você perdeu! A palavra era: {palavra}")
